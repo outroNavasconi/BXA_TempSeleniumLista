9a484e13fb94ff4d2e30333d08f5ce04fada6a17
Lucas Ramos Navasconi <lramosna@emeal.nttdata.com>
2021-12-23 18:52:49 -0300

    Exercícios resolvidos sobre automação de testes com Selenium

--- seleniumlista/src/test/java/base/BaseTest.java
+++ seleniumlista/src/test/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import framework.driver.*;
 import java.io.IOException;
 import pages.login.LoginPage;
+import framework.time.Counter;
 import org.junit.rules.TestName;
 import framework.screenshot.Screenshot;
 
@@ -11,6 +12,7 @@ public class BaseTest {
 	private final LoginPage loginPage;
 	private final Screenshot screenshot;
 	private final String evidencesFolder;
+	private static final Counter counter = new Counter();
 	
 	@Rule
 	public TestName testName = new TestName();
@@ -21,18 +23,34 @@ public class BaseTest {
 		this.evidencesFolder = evidencesFolder;
 	}
 	
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		System.out.println("INICIANDO TESTES");
+		BaseTest.counter.startCounting("TESTE_CLASSE");
+	}
+	
 	@Before
-	public void before() {
+	public void before() throws Exception {
+		BaseTest.counter.startCounting(testName.getMethodName().toString());
 		DriverFactory.getDriver().manage().window().maximize();
 		this.beforeMethod();		
 		this.loginPage.goToInitialPage();
 	}
 	
 	@After
-	public void after() throws IOException {
+	public void after() throws Exception {
 		this.takeScreenshot();
 		this.afterMethod();
+		BaseTest.counter.stopCounting(testName.getMethodName().toString());
+		System.out.println("-- Teste " + testName.getMethodName().toString());
+		System.out.println("-- Tempo: " + BaseTest.counter.getFormattedDuration(testName.getMethodName().toString()));
 		DriverFactory.killDriver();
+	}
+	
+	@AfterClass
+	public static void afterClass() throws Exception {
+		BaseTest.counter.stopCounting("TESTE_CLASSE");
+		System.out.println("TEMPO TOTAL: " + BaseTest.counter.getFormattedDuration("TESTE_CLASSE"));
 	}
 	
 	public void afterMethod() {}
